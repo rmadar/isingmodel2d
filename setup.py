c738205309a5259aa480c7e5bf23adbddb6b99ee
rmadar <romain.madar@gmail.com>
2020-10-12 16:21:59 +0200

    Turning into a pipable python module

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='ising2d',
+      version='0.1',
+      description='Simulate Ising model in 2 dimensions',
+      packages=['ising'],
+      author_email='romain.madar@cern.ch',
+      author='Romain Madar',
+)
